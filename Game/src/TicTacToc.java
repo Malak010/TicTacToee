ab604ff493bad8669e7223f13cf39ec5f8c4eb56
Malak010 <malakabdullah005@gmail.com>
2022-12-11 09:55:59 +0400

    Games

---
+++ Game/src/TicTacToc.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author LAP-3
+ *
+ */
+public class TicTacToc {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
